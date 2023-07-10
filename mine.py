17ce1947819adff10d0113160e153a572dca2793
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2023-07-10 00:15:33 -0400

    Create mine.py

---
+++ mine.py
@@ -0,0 +1,7 @@
+# Write a line of code that prints "Hello world!".
+
+
+def hello_world():
+  # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
+  
+  return variable_name
